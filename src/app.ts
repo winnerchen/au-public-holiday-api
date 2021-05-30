2a07614e14aae4fbc580edb93f7b0ff9c8f66a7e
Henry Chen <yiheng.chen233@gmail.com>
2021-05-30 23:41:15 +1000

    updated default port

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import init from './resources/init'
 // Create Express server
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 
 init();
 
